a5f62f8e5feae7f09d3964bca36f1160dcac69ba
DmytroTarasov <dtarasov24s@gmail.com>
2022-09-30 21:35:45 +0300

    End of section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,23 +18,63 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter<boolean>();
-  model: any = {};
+  registerForm: FormGroup;
+  maxDate: Date;
+  validationErrors: string[] = [];
 
   constructor(
     private accountService: AccountService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private fb: FormBuilder,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      gender: ['male'],
+      knownAs: ['', [Validators.required]],
+      dateOfBirth: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)],
+      ],
+      confirmPassword: [
+        '',
+        [Validators.required, this.matchValues('password')],
+      ],
+    });
+    // if the 'password' changes, then the validity of the 'confirmPassword' (and the overall form) also changes
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    });
+  }
+
+  // create a custom validator
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      // control in this case will be a 'confirmPassword' control
+      // if its' value is equal to the 'password' control we return null - this means that validation is passed
+      // otherwise, we return an object with a key 'isMatching' - it will be an error
+      return control?.value === control?.parent?.controls[matchTo].value ? null : {isMatching: true};
+    };
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
+    this.accountService.register(this.registerForm.value).subscribe({
       next: (_) => {
-        this.cancel();
+        this.router.navigateByUrl('/members');
       },
       error: (error) => {
-        console.log(error);
-        this.toastr.error(error.error);
+        this.validationErrors = error;
       },
     });
   }
